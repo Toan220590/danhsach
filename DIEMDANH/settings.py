4854d0769266cc4b8ced595a3906bd3ca6a56d5d
Toan <quynhon.iot@gmail.com>
2024-03-30 14:29:21 +0700

    V6

--- DIEMDANH/settings.py
+++ DIEMDANH/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'DIEMDANH.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "postgres",
+        'USER':"postgres.pjltdwvkisgtcafmpbrw",
+        'PASSWORD':"4B7Zryu9bONLmQnW",
+        'HOST':"aws-0-ap-southeast-1.pooler.supabase.com",
+        'POST':"5432",
     }
 }
 
