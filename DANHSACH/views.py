fda7b21c7e51920787d10b56d86a2151402c9fb3
Toan <quynhon.iot@gmail.com>
2024-04-04 14:46:50 +0700

    MOI_V1

--- DANHSACH/views.py
+++ DANHSACH/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework import viewsets
+
 
 from .models import Xe, Nhan_vien
 from .serializers import XeSerializer, Nhan_vienSerializer
@@ -10,6 +13,13 @@ class XeViewSet(viewsets.ModelViewSet):
     queryset = Xe.objects.all()
     serializer_class = XeSerializer
 
+
 class Nhan_vienViewSet(viewsets.ModelViewSet):
     queryset = Nhan_vien.objects.all()
     serializer_class = Nhan_vienSerializer
+
+    @action(detail=True, methods=['get'])
+    def custom_action(self, request, pk=None):
+        filtered_data = Nhan_vien.objects.filter(xe=pk)
+        serializer = Nhan_vienSerializer(filtered_data, many=True)
+        return Response(serializer.data)
